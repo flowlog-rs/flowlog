6cba8f5c1c69d4025665663e63ed016aba9efab6
Nemo Yu <zyu379@wisc.edu>
2026-02-11 12:18:20 -0500

    (feat)[sip]: forward and backward logic

--- crates/planner/src/rule_planner/sip.rs
+++ crates/planner/src/rule_planner/sip.rs
@@ -28,7 +28,44 @@ use tracing::trace;
 impl RulePlanner {
     /// Entry point for applying SIP optimizations to the current rule plan.
     pub fn apply_sip(&mut self, catalog: &mut Catalog) {
-        unimplemented!("SIP optimization is not implemented yet");
+        let positive_atom_numbers = catalog.positive_atom_number();
+
+        // Only apply SIP if there are at least 3 positive atoms
+        if positive_atom_numbers > 2 {
+            // Left -> Right foward pass.
+            for left_atom_idx in 0..positive_atom_numbers {
+                for right_atom_idx in (left_atom_idx + 1)..positive_atom_numbers {
+                    if catalog.check_sip_pair(left_atom_idx, right_atom_idx) {
+                        trace!(
+                            "SIP: forward atom_pos{} -> atom_pos{}",
+                            left_atom_idx,
+                            right_atom_idx
+                        );
+                        self.apply_sip_premaps(catalog, (left_atom_idx, right_atom_idx));
+                        self.apply_sip_projection_semijoin(catalog, left_atom_idx, right_atom_idx);
+                        trace!("Catalog:\n{}", catalog);
+                        trace!("{}", "-".repeat(60));
+                    }
+                }
+            }
+
+            // Right -> Left backward pass.
+            for left_atom_idx in (0..positive_atom_numbers).rev() {
+                for right_atom_idx in (0..left_atom_idx).rev() {
+                    if catalog.check_sip_pair(left_atom_idx, right_atom_idx) {
+                        trace!(
+                            "SIP: backward atom_pos{} -> atom_pos{}",
+                            left_atom_idx,
+                            right_atom_idx
+                        );
+                        self.apply_sip_premaps(catalog, (left_atom_idx, right_atom_idx));
+                        self.apply_sip_projection_semijoin(catalog, left_atom_idx, right_atom_idx);
+                        trace!("Catalog:\n{}", catalog);
+                        trace!("{}", "-".repeat(60));
+                    }
+                }
+            }
+        }
     }
 
     // ------------------------------------------------------------------
